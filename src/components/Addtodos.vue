feaee12667409982c365dbc086e442f01916973b
maheshwohlig <maheshwohlig@gmail.com>
2021-12-22 09:50:02 +0530

    created

---
+++ src/components/Addtodos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input
+      type="text"
+      placeholder="Enter new Todo"
+      name="todo"
+      v-model="newTodo"
+    />
+    <button @click="add">Add new Todo</button>
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "addtodos",
+  data() {
+    return {
+      newTodo: "",
+    };
+  },
+  methods: {
+    ...mapActions(["addnewTodo"]),
+    add(e) {
+      e.preventDefault();
+      this.addnewTodo(this.newTodo);
+      this.newTodo = "";
+    },
+  },
+};
+</script>
